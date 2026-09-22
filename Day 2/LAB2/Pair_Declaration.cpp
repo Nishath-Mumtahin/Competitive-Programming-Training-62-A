cc58a81b00f912a1ab0f1d17371f6a685c2e6b65
Nishath-Mumtahin <mumtahinnishath@gmail.com>
2026-09-22 13:52:01 +0600

    Add files via upload

---
+++ Day 2/LAB2/Pair_Declaration.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>>
+using namespace std;
+
+int main() {
+    pair<int, int> p;
+    p.first = 5;
+    p.second = 10;
+
+    cout << "First Value: " << p.first << endl << "Second Value: " << p.second;
+
+    return 0;
+}
